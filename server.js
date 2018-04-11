39a9c98bfdfbaf8dab5669f8ab6a689944bcca5c
Anand <karanianandkumar@yahoo.com>
2018-04-12 01:37:24 +0530

    Loading static files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express</h1>');
     res.send({
@@ -20,4 +22,6 @@ app.get('/bad', (req, res) => {
         errorMessage: "Bad Request"
     })
 })
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running on Port 3000");
+});
