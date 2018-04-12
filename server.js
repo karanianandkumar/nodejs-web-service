ddbe9f2995318b6405ef90a5c1e1eeab62e31e9c
Anand <karanianandkumar@yahoo.com>
2018-04-12 11:49:54 +0530

    Handlebars Helpers and Partials

--- server.js
+++ server.js
@@ -7,24 +7,25 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express</h1>');
-    // res.send({
-    //     name: "Anand",
-    //     age: 25,
-    //     test: true
-    // })
+
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear(),
-        welcomMessage: "Hello welcome to my brand new page"
+        welcomeMessage: "Hello welcome to my brand new page"
     })
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: "Hello welcome to my brand new page"
     });
 })
 
