14a3165b54964a28ca385735c92ceb1729f745c8
Anand <karanianandkumar@yahoo.com>
2018-04-12 11:32:38 +0530

    Using Templating Engine to load views (HBS)

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: "Anand",
-        age: 25,
-        test: true
+    // res.send({
+    //     name: "Anand",
+    //     age: 25,
+    //     test: true
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomMessage: "Hello welcome to my brand new page"
     })
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page..')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 })
 
 app.get('/bad', (req, res) => {
