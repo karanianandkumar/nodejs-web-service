6b3a44858f04e47b0f4620e3301892aa875461be
Anand <karanianandkumar@yahoo.com>
2018-04-12 23:49:12 +0530

    Express Middleware --> Maintainence check

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+const fs = require('fs');
+
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method}  ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append server.log');
+        }
+    });
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintainence.hbs');
+// })
+
 app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
